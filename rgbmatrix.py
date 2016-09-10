aa7d00dede07b82d30b20781d0f327f2bc092294
Duality <201292@live.nl>
2016-09-10 16:22:16 +0000

    quick commit with removing something

--- rgbmatrix.py
+++ rgbmatrix.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python
 import cffi
 
-matrix_prototypes = \
-"""
-    struct RGBLedMatrix;
-    struct LedCanvas;
-    struct RGBLedMatrix *led_matrix_create(int rows, int chained, int parallel);
-    void led_matrix_delete(struct RGBLedMatrix *matrix);
-    struct LedCanvas *led_matrix_get_canvas(struct RGBLedMatrix *matrix);
-    void led_canvas_get_size(const struct LedCanvas *canvas,
-                             int *width, int *height);
-    void led_canvas_set_pixel(struct LedCanvas *canvas, int x, int y,
-                              uint8_t r, uint8_t g, uint8_t b);
-    void led_canvas_clear(struct LedCanvas *canvas);
-    void led_canvas_fill(struct LedCanvas *canvas, uint8_t r, uint8_t g, uint8_t b);
-    struct LedCanvas *led_matrix_create_offscreen_canvas(struct RGBLedMatrix *matrix);
-    struct LedCanvas *led_matrix_swap_on_vsync(struct RGBLedMatrix *matrix,
-                                               struct LedCanvas *canvas);
-"""
-
 prototypes = ""
 with open('matrix/include/led-matrix-c.h') as f:
     prototypes = [line.strip() for line in f.readlines()
