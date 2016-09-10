61560ed70a72970b3bf524bfdfec412ecc8767fa
Duality <201292@live.nl>
2016-09-10 23:55:15 +0000

    fixed some code in permance python script. results so far stunning

--- performance_test.py
+++ performance_test.py
@@ -7,18 +7,18 @@ import time
 matrix = Matrix(32, 1, 1)
 canvas = Canvas(matrix)
 w, h = canvas.get_size()
-loops = 1024
+loops = 2 ** 16
 start = time.time()
 
 for i in range(0, loops):
-    col = i
+    col = i % 0xFF
     for y in range(0, h):
         for x in range(0, w):
             canvas.set_pixel(x, y, col, 0, 0)
 
 duration = time.time() - start
 
-pixel = w * h * loops
+pixels = w * h * loops
 pixels_per_sec = pixels / duration
 print("%d pixels, %dms; %.1f MegaPixels/s; %.1fHz frame update rate" %
       (pixels, 1000.0 * duration, pixels_per_sec / 1e6, loops / duration))
