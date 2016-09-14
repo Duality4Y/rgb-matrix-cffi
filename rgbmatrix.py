9272e587e09d0404958d6bc1f4f4aaed2a61ab51
Duality <201292@live.nl>
2016-09-15 01:39:52 +0200

    added function to remove multi line comments from c/c++ sources.

--- rgbmatrix.py
+++ rgbmatrix.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import cffi
 import sys
+import re
 
 prototypes = ""
+# remove the bulk of the comments and things we don't want.
 with open('matrix/include/led-matrix-c.h') as f:
     prototypes = [line.strip() for line in f.readlines()
                        if line.strip() and not
@@ -11,8 +13,32 @@ with open('matrix/include/led-matrix-c.h') as f:
                        line.startswith('}') and not
                        line.startswith('extern "C"')]
 
-prototypes = "\n".join(prototypes)
-# print("prototypes: \n%s" % prototypes)
+# http://stackoverflow.com/questions/241327/python-snippet-to-remove-c-and-c-comments
+def comment_remover(text):
+    def replacer(match):
+        s = match.group(0)
+        if s.startswith('/'):
+            return " " # note: a space and not an empty string
+        else:
+            return s
+    pattern = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE
+    )
+    return re.sub(pattern, replacer, text)
+
+# removes rest of those pesky comments. :)
+prototypes = comment_remover("\n".join(prototypes)).split('\n')
+# make nice and presentable
+prototypes = "\n".join([line.strip() for line in prototypes if line.strip()])
+# print(prototypes)
+
+"""
+    notice that cdef removes moste comments by it's self.
+    but # directives because it can't handle those yet.
+
+    but clean the source anyway as to be sure it works.
+"""
 
 ffi = cffi.FFI()
 lib = ffi.dlopen('matrix/lib/librgbmatrix.so.1')
